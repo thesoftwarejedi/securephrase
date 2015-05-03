b16aec73945b2d5870b16d464009ab54ca797aea
Dana Hanna <dana.hanna@gmail.com>
2015-05-03 14:20:29 -0400

    grunt bootstrap support

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
             }
         },
         uglify: {
-            bower: {
+            all: {
                 options: {
                     mangle: true,
                     compress: true
@@ -20,11 +20,29 @@ module.exports = function(grunt) {
                     'js/components.min.js': 'js/components.js'
                 }
             }
+        },
+        copy: {
+            all: {
+                files: [
+                    {
+                        cwd: 'bower_components/bootstrap/dist/css/',  // set working folder / root to copy
+                        src: '**/*',           // copy all files and subfolders
+                        dest: 'css/',    // destination folder
+                        expand: true           // required when using cwd
+                    },
+                    {
+                        cwd: 'bower_components/bootstrap/dist/fonts/',  // set working folder / root to copy
+                        src: '**/*',           // copy all files and subfolders
+                        dest: 'fonts/',    // destination folder
+                        expand: true           // required when using cwd
+                    }
+                ]
+            }
         }
     });
 
     require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('default', ['bower_concat', 'uglify:bower']);
+    grunt.registerTask('default', ['bower_concat', 'copy', 'uglify']);
 
 };
