fba69ee061f4253a029d006ced766c1d9ea46891
Dana Hanna <dana.hanna@gmail.com>
2015-05-04 01:41:26 -0400

    both pages working as desired, needs prettified

--- js/bip39.js
+++ js/bip39.js
@@ -31,10 +31,10 @@ function generateShares() {
     var divShares = $('#div1Shares');
     for (var i = 0; i < shares.length; i++) {
         shares[i] = shares[i].substring(1);
-        divShares.append('<span class="share-text">' + hexToBase64(shares[i]) + '</span><div class="share-qr-code" id="share' + i + '"></div>');
+        divShares.append('<span class="share-text">' + hex2b64(shares[i]) + '</span><div class="share-qr-code" id="share' + i + '"></div>');
     }
     for (var i = 0; i < shares.length; i++) {
-        new QRCode(document.getElementById('share' + i), { text: hexToBase64(shares[i]), correctLevel: QRCode.CorrectLevel.H });
+        new QRCode(document.getElementById('share' + i), { text: hex2b64(shares[i]), correctLevel: QRCode.CorrectLevel.H });
     }
 }
 
@@ -42,49 +42,10 @@ function recoverShares() {
     var shares = $('#txt2Shares').val();
     var sharesArray = shares.split('\n');
     for (var i = 0; i < sharesArray.length; i++) {
-        sharesArray[i] = '8' + base64ToHex(sharesArray[i]);
+        sharesArray[i] = '8' + b642hex(sharesArray[i]);
     }
     var hexSecret = secrets.combine(sharesArray);
 
     var key = bitcore.HDPrivateKey.fromSeed(hexSecret);
     $('#txt2Secret').val(key.xprivkey);
-}
-
-/**
- * helper functions, no outside entry
- */
-
-
-function hexToBase64(str) {
-    var tmp = chars_from_hex(str);
-    return btoa(tmp);
-}
-
-function base64ToHex(str) {
-    var tmp = atob(str);
-    return hex_from_chars(tmp);
-}
-
-function chars_from_hex(inputstr) {
-    var outputstr = '';
-    inputstr = inputstr.replace(/^(0x)?/g, '');
-    inputstr = inputstr.replace(/[^A-Fa-f0-9]/g, '');
-    inputstr = inputstr.split('');
-    for(var i=0; i<inputstr.length; i+=2) {
-        outputstr += String.fromCharCode(parseInt(inputstr[i]+''+inputstr[i+1], 16));
-    }
-    return outputstr;
-}
-
-function hex_from_chars(inputstr) {
-    var outputstr = '';
-    var hex = "0123456789abcdef";
-    hex = hex.split('');
-    var i, n;
-    var inputarr = inputstr.split('');
-    for(var i=0; i<inputarr.length; i++) {
-        n = inputstr.charCodeAt(i);
-        outputstr += hex[(n >> 4) & 0xf] + hex[n & 0xf];
-    }
-    return outputstr;
 }
--- js/securephrase.js
+++ js/securephrase.js
@@ -14,15 +14,15 @@ function generateShares() {
     var numShares = parseInt($('#txt1NumShares').val());
     var numRequired = parseInt($('#txt1NumRequired').val());
 
-    var seedHex = secrets.str2hex(seed);
+    var seedHex = asc2hex(seed);
     var shares = secrets.share(seedHex, numShares, numRequired);
     var divShares = $('#div1Shares');
     for (var i = 0; i < shares.length; i++) {
         shares[i] = shares[i].substring(1);
-        divShares.append('<span class="share-text">' + hexToBase64(shares[i]) + '</span><div class="share-qr-code" id="share' + i + '"></div>');
+        divShares.append('<span class="share-text">' + hex2b64(shares[i]) + '</span><div class="share-qr-code" id="share' + i + '"></div>');
     }
     for (var i = 0; i < shares.length; i++) {
-        new QRCode($('#share' + i), { text: hexToBase64(shares[i]), correctLevel: QRCode.CorrectLevel.L });
+        new QRCode(document.getElementById('share' + i), { text: hex2b64(shares[i]), correctLevel: QRCode.CorrectLevel.M });
     }
 }
 
@@ -30,43 +30,9 @@ function recoverShares() {
     var shares = $('#txt2Shares').val();
     var sharesArray = shares.split('\n');
     for (var i = 0; i < sharesArray.length; i++) {
-        sharesArray[i] = '8' + base64ToHex(sharesArray[i]);
+        sharesArray[i] = '8' + b642hex(sharesArray[i]);
     }
     var hexSecret = secrets.combine(sharesArray);
-    var secret = secrets.hex2str(hexSecret);
+    var secret = hex2asc(hexSecret);
     $('#txt2Secret').val(secret);
-}
-
-function hexToBase64(str) {
-    var tmp = chars_from_hex(str);
-    return btoa(tmp);
-}
-
-function base64ToHex(str) {
-    var tmp = atob(str);
-    return hex_from_chars(tmp);
-}
-
-function chars_from_hex(inputstr) {
-    var outputstr = '';
-    inputstr = inputstr.replace(/^(0x)?/g, '');
-    inputstr = inputstr.replace(/[^A-Fa-f0-9]/g, '');
-    inputstr = inputstr.split('');
-    for(var i=0; i<inputstr.length; i+=2) {
-        outputstr += String.fromCharCode(parseInt(inputstr[i]+''+inputstr[i+1], 16));
-    }
-    return outputstr;
-}
-
-function hex_from_chars(inputstr) {
-    var outputstr = '';
-    var hex = "0123456789abcdef";
-    hex = hex.split('');
-    var i, n;
-    var inputarr = inputstr.split('');
-    for(var i=0; i<inputarr.length; i++) {
-        n = inputstr.charCodeAt(i);
-        outputstr += hex[(n >> 4) & 0xf] + hex[n & 0xf];
-    }
-    return outputstr;
 }
